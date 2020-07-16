dfdb905f61b5a36e87685407d3d605a00599efe4
Getachew Mulat <gech4me@gmail.com>
2020-07-16 21:24:24 +0300

    configured

--- src/web.entry.js
+++ src/web.entry.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
+import App from "./components/App.vue";
+import './style.scss';
 
 new Vue({
   el: '#app',
-  data: {
-    msg: 'Hello World'
-  }
+  components: { App }
 });
