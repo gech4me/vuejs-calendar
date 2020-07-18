2d90740be4259ee510fdb1fd581e9f0be41e1b3d
Getachew Mulat <gech4me@gmail.com>
2020-07-18 12:45:59 +0300

    Generate list of days

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        {{ msg }}
+        <div v-for="day in days">{{ day }}</div>
     </div>
 </template>
 
@@ -9,7 +9,19 @@
     name: "App",
     data() {
       return {
-        msg: "Hey, world"
+        month: 7,
+        year: 2020,
+      }
+    },
+    computed: {
+      days() {
+        let days = [];
+        let currentDay = this.$moment(`${this.year}-${this.month}-1`,'YYYY-M-D');
+        do {
+          days.push(currentDay);
+          currentDay = this.$moment(currentDay).add(1,'days');
+        } while ((currentDay.month() + 1) === this.month);
+        return days;
       }
     },
     created() {
