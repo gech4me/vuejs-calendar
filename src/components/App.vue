9dcbd61c3c5560c624c489eb74efc1ee458e82b1
Getachew Mulat <gech4me@gmail.com>
2020-07-18 12:52:17 +0300

    adding previous days to start

--- src/components/App.vue
+++ src/components/App.vue
@@ -15,12 +15,23 @@
     },
     computed: {
       days() {
+        // Generating all days for current month.
         let days = [];
         let currentDay = this.$moment(`${this.year}-${this.month}-1`,'YYYY-M-D');
         do {
           days.push(currentDay);
           currentDay = this.$moment(currentDay).add(1,'days');
         } while ((currentDay.month() + 1) === this.month);
+
+        // Add previous days to start
+        currentDay = this.$moment(days[0]);
+        const SUNDAY = 0;
+        const MONDAY = 1;
+        do {
+          currentDay = this.$moment(currentDay).subtract(1, 'days');
+          days.unshift(currentDay);
+        } while (currentDay.day() !== MONDAY);
+
         return days;
       }
     },
