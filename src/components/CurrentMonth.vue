5fcc38c9452e5c341b9ebcdf814932fcadbee5a2
Getachew Mulat <gech4me@gmail.com>
2020-07-19 11:53:51 +0300

    restructure

--- src/components/CurrentMonth.vue
+++ src/components/CurrentMonth.vue
@@ -21,20 +21,20 @@
       },
     },
     methods: {
-      inc() {
-        if(this.month === 12) {
-          this.$store.commit('setCurrentYear',this.year + 1);
-          this.$store.commit('setCurrentMonth',1);
-        } else {
-          this.$store.commit('setCurrentMonth',this.month + 1);
-        }
-      },
       dec() {
         if(this.month === 1) {
-          this.$store.commit('setCurrentYear',this.year - 1);
           this.$store.commit('setCurrentMonth',12);
+          this.$store.commit('setCurrentYear',this.year - 1);
         } else {
           this.$store.commit('setCurrentMonth',this.month - 1);
+        }
+      },
+      inc() {
+        if(this.month === 12) {
+          this.$store.commit('setCurrentMonth',1);
+          this.$store.commit('setCurrentYear',this.year + 1);
+        } else {
+          this.$store.commit('setCurrentMonth',this.month + 1);
         }
       }
     }
