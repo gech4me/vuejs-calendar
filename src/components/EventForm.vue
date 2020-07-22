49a1bd2d2d2e2a2ecb1205c6505aacfd1cb35ab0
Getachew Mulat <gech4me@gmail.com>
2020-07-22 23:29:25 +0300

    adding input to event-form

--- src/components/EventForm.vue
+++ src/components/EventForm.vue
@@ -1,6 +1,9 @@
 <template>
     <div id="event-form" :class="{ active: active }" :style="{ top: top, left: left }">
         <h4>Add an event</h4>
+        <div class="text">
+            <input type="text" v-model="description">
+        </div>
         <button id="close-button" @click="close">&#10005</button>
     </div>
 </template>
@@ -8,6 +11,11 @@
 <script>
   export default {
     name: "EventForm",
+    data() {
+      return {
+        description: ''
+      }
+    },
     computed: {
       active() {
         return this.$store.state.eventFormActive;
