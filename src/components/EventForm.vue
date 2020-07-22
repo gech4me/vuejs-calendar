b9ad8a353691d0a2445a3663064dd2a4083711fa
Getachew Mulat <gech4me@gmail.com>
2020-07-22 23:51:35 +0300

    focus directive, enter keyup is create

--- src/components/EventForm.vue
+++ src/components/EventForm.vue
@@ -2,7 +2,7 @@
     <div id="event-form" :class="{ active: active }" :style="{ top: top, left: left }">
         <h4>Add an event</h4>
         <div class="text">
-            <input type="text" v-model="description" placeholder="Dinner at Sheraton Addis">
+            <input type="text" v-focus v-model="description" placeholder="Dinner at Sheraton Addis" @keyup.enter="create">
         </div>
         <div class="buttons">
             <button @click="create">Create</button>
@@ -39,6 +39,13 @@
           this.$store.commit('addEvent', this.description);
           this.description = "";
           this.$store.commit('eventFormActive',false);
+        }
+      }
+    },
+    directives: {
+      focus: {
+        update(el) {
+          el.focus();
         }
       }
     }
