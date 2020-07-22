74ee205c8752de017ecee8636f0b36bf325c07e0
Getachew Mulat <gech4me@gmail.com>
2020-07-22 23:26:21 +0300

    creating events state in store

--- src/components/CalendarDay.vue
+++ src/components/CalendarDay.vue
@@ -21,12 +21,7 @@
         }
       },
       events() {
-        let mockData = [
-          { description: 'random event 1', date: this.$moment('2020-07-23', 'YYYY-MM-DD') },
-          { description: 'random event 2', date: this.$moment('2020-07-24', 'YYYY-MM-DD')},
-          { description: 'random event 3', date: this.$moment('2020-07-25', 'YYYY-MM-DD')},
-        ];
-        return mockData.filter(event => event.date.isSame(this.day,'day'));
+        return this.$store.state.events.filter(event => event.date.isSame(this.day,'day'));
       }
     },
     methods: {
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import moment from 'moment-timezone';
+moment.tz.setDefault('UTC');
 
 Vue.use(Vuex);
 
@@ -9,7 +11,12 @@ export default new Vuex.Store({
     currentMonth: 7,
     eventFormPosX: 0,
     eventFormPosY: 0,
-    eventFormActive: false
+    eventFormActive: false,
+    events: [
+      { description: 'random event 1', date: moment('2020-07-23', 'YYYY-MM-DD') },
+      { description: 'random event 2', date: moment('2020-07-24', 'YYYY-MM-DD')},
+      { description: 'random event 3', date: moment('2020-07-25', 'YYYY-MM-DD')},
+    ]
   },
   mutations: {
     setCurrentMonth(state, payload) {
